50302e3266fc5d0485d5fe5a1c2af8483b7471fa
it-door <tedchu4@gmail.com>
2020-09-05 00:58:25 -0700

    Posting the night's work

---
+++ Faust.py
@@ -0,0 +1,72 @@
+import discord
+import Move
+from random import randint
+from discord.ext import tasks
+from time import sleep
+
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+
+token = read_token()
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+def format_message(guessed_right, got_hit, move_name, move_block):
+    return "{right} blocked {move} {block}, {conjunction} {wrong} {plural} playing on delay-based netcode!".format(
+        move=move_name,
+        block=move_block,
+        conjunction=("and" if guessed_right == got_hit or got_hit == 0 else "but"),
+        right=("No one" if guessed_right == 0 else guessed_right),
+        wrong=("no one" if got_hit == 0 else got_hit),
+        plural=("are" if got_hit > 1 else "is"),
+    )
+
+
+async def results(channel, message_id, move_name, move_block):
+    message = await channel.fetch_message(message_id)
+    back = 0
+    down_back = 0
+    for reaction in message.reactions:
+        if reaction.emoji == "⬅":
+            back = reaction.count - 1
+        if reaction.emoji == "↙":
+            down_back = reaction.count - 1
+    guessed_right = back if move_block == "high" else down_back
+    got_hit = down_back if move_block == "high" else back
+    message = "Everyone tried to hold forward and got IK'd. Sadge"
+    if guessed_right > 0 or got_hit > 0:
+        message = format_message(guessed_right, got_hit, move_name, move_block)
+    if move_block == "unblockable":
+        message = "{move} is unblockable online, try again with better netcode".format(
+            move=move_name
+        )
+    await channel.send(message)
+
+
+@tasks.loop(seconds=20.0)
+async def what_could_this_be():
+    await client.wait_until_ready()
+    channel = client.get_channel(298331347322404864)
+    move = Move.list[randint(0, len(Move.list) - 1)]
+    bot_message = await channel.send(file=discord.File(move.path))
+    await channel.send(
+        "What could this be? Guess the mixup by reacting to the image above!"
+    )
+    await bot_message.add_reaction("⬅")
+    await bot_message.add_reaction("↙")
+    sleep(7.0)
+    await results(channel, bot_message.id, move.name, move.block)
+
+
+what_could_this_be.start()
+client.run(token)
---
+++ Move.py
@@ -0,0 +1,23 @@
+class Move:
+    def __init__(self, name, block, path):
+        self.name = name
+        self.block = block
+        self.path = path
+
+
+list = []
+
+list.append(Move("Greed Sever", "high", "img/SPOILER_1.png"))
+list.append(Move("Stun Dipper", "low", "img/SPOILER_2.png"))
+list.append(Move("Ky 5K", "low", "img/SPOILER_3.png"))
+list.append(Move("Slayer 6P", "unblockable", "img/SPOILER_4.png"))
+list.append(Move("It's Late", "high", "img/SPOILER_5.png"))
+list.append(Move("Faust 2K", "low", "img/SPOILER_6.png"))
+list.append(Move("Jam 5K", "low", "img/SPOILER_7.png"))
+list.append(Move("Youeikyaku", "high", "img/SPOILER_8.png"))
+list.append(Move("Youzansen", "high", "img/SPOILER_9.png"))
+list.append(Move("H-Tandem Top", "unblockable", "img/SPOILER_10.png"))
+list.append(Move("Potemkin Buster", "unblockable", "img/SPOILER_11.png"))
+list.append(Move("Millia 6K", "unblockable", "img/SPOILER_12.png"))
+list.append(Move("Raven Glide", "high", "img/SPOILER_13.png"))
+list.append(Move("Raven 2K", "low", "img/SPOILER_14.png"))
